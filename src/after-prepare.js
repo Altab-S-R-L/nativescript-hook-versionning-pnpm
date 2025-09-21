92f93886e8072f00b1bb51555a9b6fe97a85f350
Azhro <dorian@altab.be>
2025-09-21 19:39:57 +0200

    chore: log resolved version info

--- src/after-prepare.js
+++ src/after-prepare.js
@@ -30,21 +30,34 @@ module.exports = function (
     hookConfig.versionCode.enabled &&
     hookConfig.versionCode.content;
   let versionCodeContent = hookConfig.versionCode.content;
+  let versionCodeEnvVar = null;
 
-  if (versionCodeContent.includes("+")) {
-    const splitted = hookConfig.versionCode.content.split("+");
-    versionCodeContent = splitted[0];
+  if (versionCodeEnabled) {
+    if (versionCodeContent.includes("+")) {
+      const splitted = hookConfig.versionCode.content.split("+");
+      versionCodeEnvVar = splitted[0];
 
-    if (!process.env[versionCodeContent]) {
+      if (!process.env[versionCodeEnvVar]) {
+        versionCodeEnabled = false;
+      } else {
+        versionCodeContent =
+          parseInt(process.env[versionCodeEnvVar], 10) + parseInt(splitted[1], 10);
+      }
+    } else if (process.env[versionCodeContent]) {
+      versionCodeEnvVar = versionCodeContent;
+      versionCodeContent = process.env[versionCodeContent];
+    } else {
+      versionCodeEnabled = false;
+    }
+  }
+
+  if (versionCodeEnabled) {
+    const parsedCode = parseInt(versionCodeContent, 10);
+    if (Number.isNaN(parsedCode)) {
       versionCodeEnabled = false;
     } else {
-      versionCodeContent =
-        parseInt(process.env[versionCodeContent]) + parseInt(splitted[1]);
+      versionCodeContent = parsedCode;
     }
-  } else if (process.env[versionCodeContent]) {
-    versionCodeContent = process.env[versionCodeContent];
-  } else {
-    versionCodeEnabled = false;
   }
 
   const platform = hookArgs.prepareData.platform;
@@ -55,6 +68,17 @@ module.exports = function (
     let manifest = new AndroidManifest().readFile(
       platformData.configurationFilePath
     );
+
+    if (hookConfig.versionName || versionCodeEnabled) {
+      const envInfo = versionCodeEnvVar
+        ? `${versionCodeEnvVar}=${process.env[versionCodeEnvVar] || 'undefined'}`
+        : 'env unused';
+      $logger.info(
+        `[nativescript-hook-versioning-pnpm] Android manifest -> versionName=${nsConfig.version}, versionCode=${
+          versionCodeEnabled ? versionCodeContent : 'disabled'
+        } (${envInfo})`
+      );
+    }
 
     if (hookConfig.versionName) {
       manifest.$("manifest").attr("android:versionName", nsConfig.version);
@@ -69,6 +93,14 @@ module.exports = function (
     let plist = iOSPList.parse(
       fs.readFileSync(platformData.configurationFilePath, "utf8")
     );
+
+    if (hookConfig.versionName || versionCodeEnabled) {
+      $logger.info(
+        `[nativescript-hook-versioning-pnpm] iOS Info.plist -> versionName=${nsConfig.version}, versionCode=${
+          versionCodeEnabled ? versionCodeContent : 'disabled'
+        }`
+      );
+    }
 
     if (hookConfig.versionName) {
       plist.CFBundleShortVersionString = nsConfig.version;
